a934b23f5018b382c0c4d9a8e0ca1ac7f59a7308
Barthor <abmatrai@gmail.com>
2018-03-21 12:11:20 -0400

    I do what I want

---
+++ cmake-build-debug/CMakeFiles/AdaptivePQ.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AdaptivePQ.dir/main.cpp.o"
+  "AdaptivePQ.pdb"
+  "AdaptivePQ"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdaptivePQ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
